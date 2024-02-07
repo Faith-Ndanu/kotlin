a002c96b74c950d07dfd7a587080ad0c91672d73
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-02-07 07:30:18 +0300

    added file

---
+++ Main.kt
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var studentName = "faith munyao"
+    println(studentName)
+    var age = 20
+    println(age)
+    var phoneNumber = +254790902921
+    println(phoneNumber)
+    var weight = 53.6
+    println(weight)
+    var nationality = "kenyan"
+    println(nationality)
+    val citizen = true
+    val nonCitizen = false
+}
